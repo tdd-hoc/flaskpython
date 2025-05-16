c3e4f282e8203d9df717f6fa4fd8273848b5119c
onbaitapbuoi5 <thachdateduy@gmail.com>
2025-05-16 13:31:12 +0700

    sưa main

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,8 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
-def getidUserID():
-    
 @app.route("/contacts", methods=["GET"])
+
 def get_contacts():
     contacts = Contact.query.all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
